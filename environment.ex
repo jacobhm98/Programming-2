af29b0a046bdc58426b4964bfa9114f2133dca4b
jacobhm98 <jacmalm@kth.se>
2019-06-12 15:18:31 +0200

    env + test file

--- environment.ex
+++ environment.ex
@@ -7,11 +7,20 @@ defmodule Env do
 		env ++ [{id, str}]
 	end
 
-	def lookup(id, env) do
-		
+	def lookup(id, [{id, str}|rest]) do
+		[{id, str}|lookup(id, rest)]
+	end
+
+	def lookup(id, [_h|t]) do
+		lookup(id, t)
+	end
+
+	def lookup(_id, []) do
+		:nil
 	end
 
 	def remove(ids, env) do
-		
+		List.foldr(ids, env, 
+			fn(id, env) -> List.keydelete(env, id, 0) end)
 	end
 end
--- q1.ex
+++ q1.ex
@@ -26,6 +26,38 @@ defmodule Q1 do
 		list({atom, freq - 1}, tempList ++ [atom])
 	end
 
+	def drop([], _) do [] end
+
+	def drop([h|t], {x, n}) do
+		cond do
+			rem(x,n) == 0 ->
+				drop(t, {x+1, n})
+
+			true -> [h|drop(t, {x+1, n})]
+		end
+	end
+
+	def drop([h|t], n) do
+		cond do
+			n == 1 ->
+				[]
+
+			true ->
+				[h|drop(t, {1, n})]
+		end
+	end
+
+	def rot([h|t], 0) do
+		append(t, reverse(h))
+	end
+
+	def rot([h|t], n) do
+		l1 = [h|l]
+		rot([l1|t], n-1)
+	end
+
+	
+
 	
 
 end
